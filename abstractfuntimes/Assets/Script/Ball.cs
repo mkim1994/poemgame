189cd889f316047d2ab07f36b9ea5299fdcc5619
mkim1994 <mkim2@andrew.cmu.edu>
2015-04-01 18:14:23 -0400

    testin

---
+++ abstractfuntimes/Assets/Script/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+
+	Animation animation = GetComponent<Animation>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey ("w")){
+			animation.Play("balltest");
+		}
+	}
+}
