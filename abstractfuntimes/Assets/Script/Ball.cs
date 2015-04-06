449111f6c8688e9848e1afbc23e9c77d92242bc2
mkim1994 <mkim2@andrew.cmu.edu>
2015-04-06 10:46:20 -0400

    figured out animation in realtestscene2!!!!

--- abstractfuntimes/Assets/Script/Ball.cs
+++ abstractfuntimes/Assets/Script/Ball.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Ball : MonoBehaviour {
 
 
-	Animation animation = GetComponent<Animation>();
+	//Animation animation = GetComponent<Animation>();
 	// Use this for initialization
 	void Start () {
 	
@@ -12,8 +12,8 @@ public class Ball : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey ("w")){
+		/*if(Input.GetKey ("w")){
 			animation.Play("balltest");
-		}
+		}*/
 	}
 }
